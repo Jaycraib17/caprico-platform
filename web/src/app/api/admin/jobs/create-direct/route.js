1b4f811206eb0a727317187b7cd22a78bdb9c398
Jaycraib17 <144291862+Jaycraib17@users.noreply.github.com>
2026-06-01 18:46:05 -0500

    Fix create direct route imports

--- web/src/app/api/admin/jobs/create-direct/route.js
+++ web/src/app/api/admin/jobs/create-direct/route.js
@@ -1,5 +1,5 @@
-import { requireAdmin } from '../../../utils/adminAuth';
-import { createDirectJob, jsonNoCache } from '../../../utils/directJobImporter';
+import { requireAdmin } from '../../../utils/adminAuth.js';
+import { createDirectJob, jsonNoCache } from '../../../utils/directJobImporter.js';
 
 export const dynamic = 'force-dynamic';
 export const revalidate = 0;
